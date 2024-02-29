03cf8d870a2ae1d6f5523cd48032f5dec88d132a
advaithbalram <advaithbalram@gmail.com>
2024-02-29 20:55:29 +0530

    add backend url

--- frontend/lib/Constants/api.dart
+++ frontend/lib/Constants/api.dart
@@ -1,2 +1,3 @@
-const String api = 'http://127.0.0.1:8000/apis/v1/';
+//const String api = 'http://127.0.0.1:8000/apis/v1/';
 //const String api = 'http://10.0.2.2:8000/apis/v1/';
+const String api = 'https://abn10.pythonanywhere.com/apis/v1/';
